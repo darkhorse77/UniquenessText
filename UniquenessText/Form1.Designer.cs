d802ab86b171e29d580c07c922cdc1eed125bffa
Vladimir <vladimir.kovalyov2002@gmail.com>
2018-10-09 15:40:53 +0500

    First Commit
    Simple program

---
+++ UniquenessText/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace UniquenessText
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.replaceButton = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(27, 63);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(387, 342);
+            this.textBox.TabIndex = 0;
+            // 
+            // replaceButton
+            // 
+            this.replaceButton.Location = new System.Drawing.Point(176, 416);
+            this.replaceButton.Name = "replaceButton";
+            this.replaceButton.Size = new System.Drawing.Size(75, 23);
+            this.replaceButton.TabIndex = 1;
+            this.replaceButton.Text = "Replace!";
+            this.replaceButton.UseVisualStyleBackColor = true;
+            this.replaceButton.Click += new System.EventHandler(this.replaceButton_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(27, 31);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(193, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Русские символы на английские";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(226, 31);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(193, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Английские символы на русские";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 456);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.replaceButton);
+            this.Controls.Add(this.textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UniquenessText [RU-ENG] v 0.3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button replaceButton;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ UniquenessText/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace UniquenessText
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        static readonly Dictionary<char, char> ConvertChar =
+            new Dictionary<char, char>
+            {
+                {'а', 'a'},
+                {'А', 'A'},
+                {'у', 'y'},
+                {'К', 'K'},
+                {'е', 'e'},         // первое значение - русский символ
+                {'Е', 'E'},         //
+                {'Н', 'H'},         // второе - английский
+                {'О', 'O'},         //
+                {'о', 'o'},
+                {'Х', 'X'},
+                {'х', 'x'},
+                {'В', 'B'},
+                {'р', 'p'},
+                {'Р', 'P'},
+                {'с', 'c'},
+                {'С', 'C'},
+                {'M', 'M'},
+                {'T', 'T'},
+            };
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void replaceButton_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                foreach (var charPair in ConvertChar)
+                {
+                    textBox.Text = textBox.Text.Replace(charPair.Key, charPair.Value);
+                }
+            }
+            else if (radioButton2.Checked == true)
+            {
+                foreach (var charPair in ConvertChar)
+                {
+                    textBox.Text = textBox.Text.Replace(charPair.Value, charPair.Key);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите тип замены!", "Ошбика!");
+            }
+        }
+    }
+}
